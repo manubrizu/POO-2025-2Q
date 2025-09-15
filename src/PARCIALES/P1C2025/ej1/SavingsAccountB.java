e18dff6b3ce0ee9c72a4f5fefc6a98ec0304dbb4
Manuel Brizuela <manuel.brizuela@philips.edu.ar>
2025-09-15 20:21:51 -0300

    1P1C2025

---
+++ src/PARCIALES/P1C2025/ej1/SavingsAccountB.java
@@ -0,0 +1,22 @@
+package PARCIALES.P1C2025.ej1;
+
+import java.time.MonthDay;
+
+public class SavingsAccountB extends SavingsAccount {
+    protected MonthDay lastExtractDate;
+
+    SavingsAccountB(MonthDay openDate){
+        super(openDate);
+        this.lastExtractDate = openDate;
+    }
+
+    @Override
+    public void withdrawFunds(double extractAmount, MonthDay extractDate){
+        if(extractAmount < 0 || extractDate.isBefore(openDate) || extractDate.isBefore(lastExtractDate)){
+            throw new IllegalArgumentException("puto");
+        }
+        this.lastExtractDate = extractDate;
+        this.saldo -= extractAmount;
+    }
+
+}
---
+++ src/PARCIALES/P1C2025/ej1/SavingsAccountC.java
@@ -0,0 +1,31 @@
+package PARCIALES.P1C2025.ej1;
+
+import java.time.MonthDay;
+
+public class SavingsAccountC extends SavingsAccountB{
+    private int MAX_INTEREST = 1_000_000;
+
+    SavingsAccountC(MonthDay openDate){
+        super(openDate);
+    }
+
+    @Override
+    public void depositFunds(double deposit, MonthDay depositDate){
+        if(deposit < 100_000 || depositDate.isBefore(openDate)){
+            throw new IllegalArgumentException("puto");
+        }
+        this.saldo += deposit;
+    }
+
+    @Override
+    public void interestPayment(int days){
+        for (int i = 0; i < days; i++){
+            if(saldo >= MAX_INTEREST){
+                this.saldo += MAX_INTEREST * INTEREST;
+            }
+            else {
+                this.saldo += this.saldo * INTEREST;
+            }
+        }
+    }
+}
